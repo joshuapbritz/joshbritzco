63d2d888af4a176e488a0a38a38eb7c487cc3cde
Joshua Britz <joshua@onepointfour.io>
2019-02-08 15:57:20 +0200

    Deploy

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -3,14 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    var name = "JOSHUA";
+    const name = 'JOSHUA';
   }
-
 }
