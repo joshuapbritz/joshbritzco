492f98512d7f62e60285221b6fad0cf6551ffb6e
Joshua Britz <joshua@onepointfour.io>
2019-05-17 14:57:03 +0200

    Bug Fixes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 My personal website
+
+https://colorable.jxnblk.com/000266/00ffa6
--- src/app/pages/skills/skills.module.ts
+++ src/app/pages/skills/skills.module.ts
@@ -1,8 +1,8 @@
-import { AppTimeline } from './../../components/timeline/timeline.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SkillsComponent } from './skills.component';
 import { Routes, RouterModule } from '@angular/router';
+import { AppTimeline } from 'src/app/components/timeline/timeline.module';
 
 const routes: Routes = [{ path: '', component: SkillsComponent }];
 
