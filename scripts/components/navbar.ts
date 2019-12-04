c71954c737cc0b41328a908bad72294514d88dbc
Joshua Britz <joshua@onepointfour.io>
2019-12-04 18:57:40 +0200

    Fixes

--- scripts/components/navbar.ts
+++
@@ -1,7 +0,0 @@
-((nav: HTMLElement) => {
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 10) {
-      nav.classList.add('scrolled');
-    } else nav.classList.remove('scrolled');
-  });
-})(document.querySelector('nav'));
