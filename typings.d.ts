0d4ecac80e5591d1dc2de7f03a0c6962128cfec5
Joshua Britz <joshua@onepointfour.io>
2020-07-21 10:38:45 +0200

    Yers

--- typings.d.ts
+++
@@ -1,3 +0,0 @@
-interface Object {
-  fromEntries(args): any;
-}
--- workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  globDirectory: 'dist/',
-  globPatterns: ['**/*.{ico,html,js,css,svg,jpg,jpeg,png,json,txt}'],
-  swSrc: './sw.js',
-  swDest: 'dist/sw.js',
-  injectionPointRegexp: /(const precacheManifest = )\[\](;)/,
-};
